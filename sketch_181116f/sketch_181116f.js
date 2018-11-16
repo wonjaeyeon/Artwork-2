8c4f7b24d6c412d3a9640423beeaee4275bf785c
wonjaeyeon <44885604+wonjaeyeon@users.noreply.github.com>
2018-11-16 10:50:46 +0900

    Add files via upload

---
+++ sketch_181116f/sketch_181116f.js
@@ -0,0 +1,63 @@
+
+function setup() {
+  createCanvas(1500, 1000);
+  background(255);
+
+   
+   
+
+}
+   
+   
+
+
+function draw() {
+   stroke(0);
+strokeWeight(10);
+line(0,150,1500,150);
+
+
+stroke(0);
+strokeWeight(10);
+line(0,400,1500,400);
+
+
+stroke(0);
+strokeWeight(10);
+line(0,700,1500,700);
+
+
+stroke(0);
+strokeWeight(10);
+line(300,0,300,1000);
+
+
+stroke(0);
+strokeWeight(10);
+line(400,0,400,1000);
+
+
+stroke(0);
+strokeWeight(10);
+line(800,0,800,1000);
+
+
+stroke(0);
+strokeWeight(10);
+line(1200,0,1200,1000);
+
+
+
+fill(255,0,0);
+rect(300,150,100,250,8);
+
+
+fill(255,255,0);
+rect(300,600,100,100,8);
+
+
+fill(0,0,255);
+rect(800,150,400,250,8);
+
+  
+}
